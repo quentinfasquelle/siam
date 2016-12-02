5c26d28a925cc8a758deda9fd9a2c4a419b32467
Spriithy <theo.dano@gmail.com>
2016-12-02 16:47:29 +0100

    enumerations utiles

---
+++ siam/direction.h
@@ -0,0 +1,6 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+enum Direction { HAUT, BAS, DROITE, GAUCHE };
+
+#endif  // DIRECTION_H
---
+++ siam/typePion.h
@@ -0,0 +1,6 @@
+#ifndef TYPEPION_H
+#define TYPEPION_H
+
+enum TypePion { VIDE, MONTAGNE, ELEPHANT, RHINOCEROS };
+
+#endif  // TYPEPION_H
